31158e89254676074c36a4f112aa14a6f7af3ef3
jphieno <jari-pekka.heino@outlook.com>
2021-10-25 21:04:07 +0300

    added gravity and option to jump. jumo doesnt not work tho yet. working on it

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/Ground.png').convert()
 
-score_surf = test_font.render('My game', False, 'Black')
+score_surf = test_font.render('My game', False, (64,64,64))
 score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -18,31 +18,37 @@ snail_rect = snail_surf.get_rect(bottomright =(600,300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom =(80,300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    #if event.type == pygame.MOUSEMOTION:
-    #    if player_rect.collidepoint(event.pos): print('collision')
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if player_rect.collidepoint(event.pos):
+            player_gravity = -20
+        
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            player_gravity = -20
+        
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    pygame.draw.rect(screen,'#c0e8ec',score_rect)
+    pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
     screen.blit(score_surf,score_rect)
 
     snail_rect.x -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
+    if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(snail_surf,(snail_rect))
     screen.blit(player_surf,player_rect)
 
-    #if player_rect.colliderect(snail_rect):
-    #    print('collision')
-
-    #mouse_pos = pygame.mouse.get_pos()
-    #if player_rect.collidepoint(mouse_pos):
-    #    print(pygame.mouse.get_pressed())        
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
+    screen.blit(player_surf,player_rect)     
 
     pygame.display.update()
     clock.tick(60)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    #if player_rect.colliderect(snail_rect):
